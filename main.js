9293f5804fd2dedc55351e576a85abc08806b345
Nickgaming-Hithub <nictho1@cisdbraves.org>
2024-10-28 20:32:48 +0000

    modified:   index.html 	modified:   main.js 	modified:   node_modules/.vite/deps/_metadata.json 	new file:   node_modules/.vite/deps/three_webgpu.js 	new file:   node_modules/.vite/deps/three_webgpu.js.map 	modified:   textures/grass/bottom.js 	modified:   textures/grass/sides.js 	modified:   textures/grass/top.js

--- main.js
+++ main.js
@@ -39,7 +39,11 @@ const grassBlockBottomMap = new grassBlockBottom().map();
 
 camera.rotation.order = "YXZ";
 
-camera.position.z = 5;
+var fps = 60;
+
+var fpsTick = 0;
+
+var oldFps;
 
 const screen = {
     fov: 75,
@@ -50,15 +54,22 @@ const mouse = {
     y: 0,
 };
 
+// acceleration = (force (n) * friction) / mass (kg)
+// speed = acceleration * time
+
 const player = {
+    x: 0,
+    y: 2,
+    z: 0,
     vx: 0,
     vy: 0,
     vz: 0,
     inAir: false,
     settings: {
-        speed: 0.01,
-        friction: 0.9,
+        acceleration: 0.02,
+        friction: 0.84,
         gravity: 0.006,
+        thickness: 0.2,
     },
 };
 
@@ -70,7 +81,6 @@ const objects = {
     x: [],
     y: [],
     z: [],
-    texture: [],
 };
 
 // World Objects
@@ -78,8 +88,8 @@ const objects = {
 function createObject(width, height, length, x, y, z, texture1, texture2, texture3, texture4, texture5, texture6) {
     objects.index++;
     objects.length.push(length);
-    objects.length.push(width);
-    objects.length.push(height);
+    objects.width.push(width);
+    objects.height.push(height);
     objects.x.push(x);
     objects.y.push(y);
     objects.z.push(z);
@@ -103,7 +113,40 @@ const createBlock = {
     },
 };
 createBlock.grass(0, -1, 0);
-createObject(20, 1, 20, 0, -2, 0, uvTextureMap, uvTextureMap, uvTextureMap, uvTextureMap, uvTextureMap, uvTextureMap);
+createBlock.grass(1, -1, 0);
+createBlock.grass(2, 1, 1);
+createBlock.grass(2, 0, 0);
+// createObject(20, 1, 20, 0, -2, 0, uvTextureMap, uvTextureMap, uvTextureMap, uvTextureMap, uvTextureMap, uvTextureMap);
+
+objects.index++;
+objects.length.push(21);
+objects.width.push(21);
+objects.height.push(1);
+objects.x.push(0);
+objects.y.push(-2);
+objects.z.push(0);
+let textureThing1 = new grassBlockSides().map();
+textureThing1.wrapS = THREE.RepeatWrapping;
+textureThing1.wrapT = THREE.RepeatWrapping;
+textureThing1.repeat.set(21, 1);
+let textureThing2 = new grassBlockTop().map();
+textureThing2.wrapS = THREE.RepeatWrapping;
+textureThing2.wrapT = THREE.RepeatWrapping;
+textureThing2.repeat.set(21, 21);
+let textureThing3 = new grassBlockBottom().map();
+textureThing3.wrapS = THREE.RepeatWrapping;
+textureThing3.wrapT = THREE.RepeatWrapping;
+textureThing3.repeat.set(21, 21);
+let objectThing = new THREE.Mesh(new THREE.BoxGeometry(21, 1, 21), [
+    new THREE.MeshBasicMaterial({map: textureThing1,transparent:true, side:THREE.DoubleSide}), //right side
+    new THREE.MeshBasicMaterial({map: textureThing1,transparent:true, side:THREE.DoubleSide}), //left side
+    new THREE.MeshBasicMaterial({map: textureThing2,transparent:true, side:THREE.DoubleSide}), //top side
+    new THREE.MeshBasicMaterial({map: textureThing3,transparent:true, side:THREE.DoubleSide}), //bottom side
+    new THREE.MeshBasicMaterial({map: textureThing1,transparent:true, side:THREE.DoubleSide}), //front side
+    new THREE.MeshBasicMaterial({map: textureThing1,transparent:true, side:THREE.DoubleSide}), //back side
+]);
+objectThing.position.set(0, -2, 0);
+scene.add(objectThing);
 
 // Camera Controls
 
@@ -155,71 +198,81 @@ function loop() {
 
     camera.updateProjectionMatrix();
 
-    // Movement Detection
+    // Movement
 
     if(keypressed.has("KeyW")) {
-        player.vx -= Math.sin(camera.rotation.y) * player.settings.speed;
-        player.vz -= Math.cos(camera.rotation.y) * player.settings.speed;
+        player.vx -= Math.sin(camera.rotation.y) * player.settings.acceleration;
+        player.vz -= Math.cos(camera.rotation.y) * player.settings.acceleration;
     };
     if(keypressed.has("KeyA")) {
-        player.vx -= Math.cos(camera.rotation.y) * player.settings.speed;
-        player.vz += Math.sin(camera.rotation.y) * player.settings.speed;
+        player.vx -= Math.cos(camera.rotation.y) * player.settings.acceleration;
+        player.vz += Math.sin(camera.rotation.y) * player.settings.acceleration;
     };
     if(keypressed.has("KeyS")) {
-        player.vx += Math.sin(camera.rotation.y) * player.settings.speed;
-        player.vz += Math.cos(camera.rotation.y) * player.settings.speed;
+        player.vx += Math.sin(camera.rotation.y) * player.settings.acceleration;
+        player.vz += Math.cos(camera.rotation.y) * player.settings.acceleration;
     };
     if(keypressed.has("KeyD")) {
-        player.vx += Math.cos(camera.rotation.y) * player.settings.speed;
-        player.vz -= Math.sin(camera.rotation.y) * player.settings.speed;
+        player.vx += Math.cos(camera.rotation.y) * player.settings.acceleration;
+        player.vz -= Math.sin(camera.rotation.y) * player.settings.acceleration;
     };
-
-    // Friction and Gravity
 
     player.vx *= player.settings.friction;
     player.vz *= player.settings.friction;
     player.vy -= player.settings.gravity;
 
-    // Movement
+    // Collisions
 
-    camera.position.x += player.vx;
-    camera.position.y += player.vy;
-    camera.position.z += player.vz;
 
-    // Jump Detection and World Floor
+    player.x += player.vx;
+    if(collisionDetection()) {
+        player.x -= player.vx;
+        player.vx = 0;
+    };
 
-    if(!player.inAir) {
-        if(keypressed.has("Space")) {
-            player.vy = 0.1;
+    player.y += player.vy;
+    if(collisionDetection()) {
+        let holdJump = -Math.abs(player.vy) == player.vy;
+        player.y -= player.vy;
+        player.vy = 0;
+        if(keypressed.has("Space") && holdJump) {
+            player.vy = 0.12;
         };
     };
 
-    if(camera.position.y < 0) {
-        camera.position.y = 0;
-        player.inAir = false;
-    } else {
-        player.inAir = true;
+    player.z += player.vz;
+    if(collisionDetection()) {
+        player.z -= player.vz;
+        player.vz = 0;
     };
 
-    // Other / Effects
-
-    if(player.inAir) {
-        player.settings.friction = 0.92;
-    } else {
-        player.settings.friction = 0.9;
+    /*
+    player.y = objects.y[i] + (objects.height[i] / 2);
+    player.vy = 0;
+    if(keypressed.has("Space")) {
+        player.vy = 0.1;
     };
+    */
+
+    // Position
+
+    camera.position.set(player.x, player.y + 1.8, player.z);
+    document.getElementById("cords").innerText = "X: " + Math.floor(player.x) + ", Y: " + Math.floor(player.y) + ", Z: " + Math.floor(player.z) + "\n FPS: " + fps;
+
+    if(player.y < -50) {
+        player.x = 0;
+        player.y = 5;
+        player.z = 0;
+        player.vy = 0;
+    };
+
+    // Other
 
     if(keypressed.has("KeyR")) {
-        player.settings.speed = 0.02;
-        screen.fov = 85;
-    } else if(keypressed.has("ShiftLeft")) {
-        if(!player.inAir) {
-            player.settings.speed = 0.003;
-            camera.position.y -= 0.2;
-            screen.fov = 70;
-        };
+        player.settings.acceleration = 0.04;
+        screen.fov = 80;
     } else {
-        player.settings.speed = 0.01;
+        player.settings.acceleration = 0.02;
         screen.fov = 70;
     };
 
@@ -228,3 +281,33 @@ function loop() {
 	renderer.render( scene, camera );
 
 };
+
+function collisionDetection() {
+    let touching = false;
+    for(let index = 0; index < 2; index += 0.1) {
+        for(let i = 0; i < (objects.index + 1); i++) {
+            if(player.y + index + 0.1 < objects.y[i] + (objects.height[i] / 2) && player.y + index + 0.1 > objects.y[i] - (objects.height[i] / 2)) {
+                if(player.x > objects.x[i] - (objects.width[i] / 2) - player.settings.thickness && player.x < objects.x[i] + (objects.width[i] / 2) + player.settings.thickness) {
+                    if(player.z > objects.z[i] - (objects.length[i] / 2) - player.settings.thickness && player.z < objects.z[i] + (objects.length[i] / 2) + player.settings.thickness) {
+                        touching = true;
+                    };
+                };
+            };
+        };
+    };
+    return touching;
+};
+
+function fpsLoop() {
+    if(fpsTick > 60) {
+        fps = new Date();
+        fps -= oldFps;
+        oldFps = new Date();
+        fpsTick = 0;
+    };
+
+    fpsTick++;
+
+    window.requestAnimationFrame(fpsLoop);
+};
+fpsLoop();
--- textures/grass/bottom.js
+++ textures/grass/bottom.js
@@ -2,7 +2,8 @@ import * as THREE from 'three';
 
 class grassBlockBottom {
     map() {
-        this.texture = new THREE.TextureLoader().load("data:image/jpeg;base64,<base64>");
+        this.texture = new THREE.TextureLoader().load("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAl5JREFUOE9Vk81OU0EUx39Tohs/uBcCbSFtkdaNCwsxsDCxwE4Mxp07E8LaF/ABfAZ36Du4AFfWykqN5Wp0ofTrkrTeCm0pGxd6x5zT3qbMZs5Mzpzz/zhj3jx/YqutU2T57R5Yw0Y+Sy3oENoQP+iDCam3OiwkpzRvPZ+l6FV0N88e37XpWYeSV9UEay2ZxBS9uXW+7r0knZhkI5+j0jrlWBoAUb7ERhBINbBUW1127q9Q9Kp6HnTLUfSONJYlSGRta14F8+LpA/uNRR4mz3h7eEQm7nLg1ckkJllbukmleYIx4Ac90nFHaQp0oS2xFpBAYOWS0+zuf6KQv4Hf7gMh6RmH0heh53I+e4dr7c8jCkJZNRBYhfyiQmsEXdXgoFwhNAPYC/MuchgXMhJTNXi1/1EFjGG5svyI8/Jr1UD4yx4pLo8kFrQR6pGIwisGHI0svUghQqhWD51QFyINsJZ03B1R8IMz6s0TMnMuCzOujMdgTsbcUA2kgNxFc4ANaQR9rA3JJFyMWDC2BOk4lWEBg9/usiaTheWdV6Nwe5Fa65T3XpXtzVVCva+QmnU1pzd/j375tSDYso12B6MUprGEZJPTlMo15uPX+R7LcosfGHsJv91RJwr5HH7QpfarG9nYVRsbQYdM3MESI5t0KR1W1UopamxMdVBCFtaWB1R0DlIzDse/e/yzfwknrnI5/EMqLlAHcyFddzZX2d37oHZHk3jBhXqzw/bmCsXyTxVRBPRbJ6SGP1DElEJyrzYOG4xsjKqKJcfBGc7SFk6zSGgm9IH8BYEkQ6Tndk8L/gdLEVrKnyItwAAAAABJRU5ErkJggg==");
+        this.texture.magFilter = THREE.NearestFilter;
         return this.texture;
     };
 };
